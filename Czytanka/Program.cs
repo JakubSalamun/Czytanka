f1dbf02bbee874a9b923f4c4daa213e36a98b9a5
Askalt <jakub.salamun98@gmail.com>
2022-03-02 12:55:57 +0100

    book 2 & book 3

--- Czytanka/Program.cs
+++ Czytanka/Program.cs
@@ -70,6 +70,7 @@ namespace Czytanka
             {
                 case "1":
                     Console.WriteLine("IT");
+                    var helpString = "IT";
                     var files =Directory.GetFiles("D:/Czytanka/IT/","*.txt",SearchOption.AllDirectories);
                     foreach (var item in files)
                     {
@@ -79,14 +80,26 @@ namespace Czytanka
                     var chapter = Console.ReadLine();
                     var documen1= File.ReadAllText($"D:/Czytanka/IT/IT-{chapter}.txt");
                     Console.WriteLine(documen1);
-                    CheckReadTome();
+                    CheckReadTome(helpString);
                     break;
 
 
 
 
                 case "2":
+              
                     Console.WriteLine("Obecność");
+                    var helpString2 = "Obecność";
+                    var files2 = Directory.GetFiles("D:/Czytanka/Obecność/", "*.txt", SearchOption.AllDirectories);
+                    foreach (var item in files2)
+                    {
+                        Console.WriteLine(String.Join(Environment.NewLine, files2));
+                    }
+                    Console.WriteLine("Podaj rozdział gdzie skończyłaś/łeś czytać (gdy zaczynasz podaj wartość 1)");
+                    var chapter2 = Console.ReadLine();
+                    var documen2 = File.ReadAllText($"D:/Czytanka/Obecność/Obecność-{chapter2}.txt");
+                    Console.WriteLine(documen2);
+                    CheckReadTome(helpString2);
                     break;
 
 
@@ -94,6 +107,17 @@ namespace Czytanka
 
                 case "3":
                     Console.WriteLine("Egzorcysta");
+                    var helpString3 = "Egzorcysta";
+                    var files3 = Directory.GetFiles("D:/Czytanka/Egzorcysta/", "*.txt", SearchOption.AllDirectories);
+                    foreach (var item in files3)
+                    {
+                        Console.WriteLine(String.Join(Environment.NewLine, files3));
+                    }
+                    Console.WriteLine("Podaj rozdział gdzie skończyłaś/łeś czytać (gdy zaczynasz podaj wartość 1)");
+                    var chapter3 = Console.ReadLine();
+                    var documen3 = File.ReadAllText($"D:/Czytanka/Egzorcysta/Egzorcysta-{chapter3}.txt");
+                    Console.WriteLine(documen3);
+                    CheckReadTome(helpString3);
                     break;
 
 
@@ -109,15 +133,15 @@ namespace Czytanka
 
         }
 
-        static void CheckReadTome()
+        static void CheckReadTome(string book)
         {
             Console.WriteLine("Co już przeczytałeś? (Podaj numer rozdziału)");
             var chapter = Console.ReadLine();
             var oldChapter = "{Rozdział x}";
             var newChapter = oldChapter.Replace('x',Convert.ToChar(chapter));
-            var templat = File.ReadAllText(@"D:\Czytanka\IT.txt");
+            var templat = File.ReadAllText($"D:/Czytanka/{book}.txt");
             var document = templat.Replace(newChapter, "Przeczytałem");
-            File.WriteAllText($"D:/Czytanka/IT-{chapter}.txt", document);
+            File.WriteAllText($"D:/Czytanka/{book}-{chapter}.txt", document);
 
         }
     }
